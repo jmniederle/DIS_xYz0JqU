8dd63956384e9b8e7d9273d41350bde766013f13
jmniederle <j.m.niederle@student.tue.nl>
2019-03-07 10:36:37 +0100

    trying query 2

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,5 +1,5 @@
 SELECT c.CourseName, creg.Grade FROM StudentRegistrationsToDegrees as sreg, CourseRegistrations as creg, CourseOffers as coffer, Courses as c where sreg.StudentId = %1% and sreg.StudentRegistrationId = creg.StudentRegistrationId and creg.Grade >= 5 and creg.CourseOfferId = coffer.CourseOfferId AND coffer.CourseId = c.CourseId AND sreg.DegreeId = %2% ORDER BY coffer.Year, coffer.Quartile, coffer.CourseOfferId;
-SELECT 0;
+WITH ExcellentStudentRegistrations AS (SELECT StudentRegistrationId FROM DegreeProgress WHERE GPA > %1% AND AcqECTS > TotalECTS EXCEPT (SELECT StudentRegistrationId FROM CourseRegistrations WHERE Grade IS NULL)) SELECT DISTINCT StudentId FROM ExcellentStudentRegistrations ESR, DegreeProgress DP WHERE ESR.StudentRegistrationId = DP.StudentRegistrationId ORDER BY StudentId;
 SELECT 0;
 SELECT 0;
 SELECT 0;
